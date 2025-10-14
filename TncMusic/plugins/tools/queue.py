d48d51d50629dcf9215515d510a192389e795119
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 20:24:02 +0530

    Update queue.py

--- TncMusic/plugins/tools/queue.py
+++ TncMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from TNCxMUSIC import app
-from TNCxMUSIC.misc import db
-from TNCxMUSIC.utils import TNCxBin, get_channeplayCB, seconds_to_min
-from TNCxMUSIC.utils.database import get_cmode, is_active_chat, is_music_playing
-from TNCxMUSIC.utils.decorators.language import language, languageCB
-from TNCxMUSIC.utils.inline import queue_back_markup, queue_markup
+from TncMusic import app
+from TncMusic.misc import db
+from TncMusic.utils import TNCxBin, get_channeplayCB, seconds_to_min
+from TncMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from TncMusic.utils.decorators.language import language, languageCB
+from TncMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
