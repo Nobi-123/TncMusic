5de1a384fbb8e238bbebdf984ced0d7d42d7adf4
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 21:12:56 +0530

    Update Soundcloud.py

--- TncMusic/platforms/Soundcloud.py
+++ TncMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from TNCxMUSIC.utils.formatters import seconds_to_min
+from TncMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
