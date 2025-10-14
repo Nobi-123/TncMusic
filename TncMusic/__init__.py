9ff004147a9274630705ae20aeba960a8b0ec911
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 19:46:52 +0530

    Update __init__.py

--- TncMusic/__init__.py
+++ TncMusic/__init__.py
@@ -1,8 +1,8 @@
-from TNCxMUSIC.core.bot import TNCx
-from TNCxMUSIC.core.dir import dirr
-from TNCxMUSIC.core.git import git
-from TNCxMUSIC.core.userbot import Userbot
-from TNCxMUSIC.misc import dbb, heroku
+from TncMusic.core.bot import TNC
+from TncMusic.core.dir import dirr
+from TncMusic.core.git import git
+from TncMusic.core.userbot import Userbot
+from TncMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
@@ -11,7 +11,7 @@ git()
 dbb()
 heroku()
 
-app = TNCx()
+app = TNC()
 userbot = Userbot()
 
 
