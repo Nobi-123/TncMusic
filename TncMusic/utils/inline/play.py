5d94f4c77036ef79fe6447798f0cdd61bb3ec510
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 19:59:35 +0530

    Update play.py

--- TncMusic/utils/inline/play.py
+++ TncMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from TNCxMUSIC.utils.formatters import time_to_seconds
+from TncMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
