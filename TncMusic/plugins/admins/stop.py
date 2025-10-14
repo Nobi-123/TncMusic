56f1a4fd9439b7f2e37534cd06d0557f0df2edbc
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 21:00:03 +0530

    Update stop.py

--- TncMusic/plugins/admins/stop.py
+++ TncMusic/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from TNCxMUSIC import app
-from TNCxMUSIC.core.call import TNCx
-from TNCxMUSIC.utils.database import set_loop
-from TNCxMUSIC.utils.decorators import AdminRightsCheck
-from TNCxMUSIC.utils.inline import close_markup
+from TncMusic import app
+from TncMusic.core.call import TNC
+from TncMusic.utils.database import set_loop
+from TncMusic.utils.decorators import AdminRightsCheck
+from TncMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await TNCx.stop_stream(chat_id)
+    await TNC.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
