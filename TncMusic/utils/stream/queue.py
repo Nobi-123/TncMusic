fa4baf81a1fcebf341942814ef336acfb1fdc763
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 19:57:13 +0530

    Update queue.py

--- TncMusic/utils/stream/queue.py
+++ TncMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from TNCxMUSIC.misc import db
-from TNCxMUSIC.utils.formatters import check_duration, seconds_to_min
+from TncMusic.misc import db
+from TncMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
