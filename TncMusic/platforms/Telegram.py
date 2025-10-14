e62626c4cea6e33a0105465d01a7abec92c71a6d
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 21:12:22 +0530

    Update Telegram.py

--- TncMusic/platforms/Telegram.py
+++ TncMusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from TNCxMUSIC import app
-from TNCxMUSIC.utils.formatters import (
+from TncMusic import app
+from TncMusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
