64b349ea84c469b86d21da74e748f7c211acc092
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 20:51:08 +0530

    Update channel.py

--- TncMusic/plugins/play/channel.py
+++ TncMusic/plugins/play/channel.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from TNCxMUSIC import app
-from TNCxMUSIC.utils.database import set_cmode
-from TNCxMUSIC.utils.decorators.admins import AdminActual
+from TncMusic import app
+from TncMusic.utils.database import set_cmode
+from TncMusic.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
