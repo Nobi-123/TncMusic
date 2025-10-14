7c99f510ece81d9086322105dd7c4544c3c79233
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 20:28:22 +0530

    Update language.py

--- TncMusic/plugins/tools/language.py
+++ TncMusic/plugins/tools/language.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-from TNCxMUSIC import app
-from TNCxMUSIC.utils.database import get_lang, set_lang
-from TNCxMUSIC.utils.decorators import ActualAdminCB, language, languageCB
+from TncMusic import app
+from TncMusic.utils.database import get_lang, set_lang
+from TncMusic.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
