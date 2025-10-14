cafd501da278c44511a21f993118cb88c9f5d416
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 19:50:03 +0530

    Update extraction.py

--- TncMusic/utils/extraction.py
+++ TncMusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from TNCxCODE import app
+from TncMusic import app
 
 
 async def extract_user(m: Message) -> User:
