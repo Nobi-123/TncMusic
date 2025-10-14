6fec04f0dce3f6c30bfdbdd78bbc8c7706005f9c
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 20:57:40 +0530

    Update settings.py

--- TncMusic/plugins/bot/settings.py
+++ TncMusic/plugins/bot/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from TNCxMUSIC import app
-from TNCxMUSIC.utils.database import (
+from TncMusic import app
+from TncMusic.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,16 +25,16 @@ from TNCxMUSIC.utils.database import (
     skip_off,
     skip_on,
 )
-from TNCxMUSIC.utils import bot_sys_stats
-from TNCxMUSIC.utils.decorators.admins import ActualAdminCB
-from TNCxMUSIC.utils.decorators.language import language, languageCB
-from TNCxMUSIC.utils.inline.settings import (
+from TncMusic.utils import bot_sys_stats
+from TncMusic.utils.decorators.admins import ActualAdminCB
+from TncMusic.utils.decorators.language import language, languageCB
+from TncMusic.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from TNCxMUSIC.utils.inline.start import private_panel
+from TncMusic.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
