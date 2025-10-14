a17066f9bd4dbe30d343b69470418338da9b1813
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 21:05:29 +0530

    Update resume.py

--- TncMusic/plugins/admins/resume.py
+++ TncMusic/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from TNCxMUSIC import app
-from TNCxMUSIC.core.call import TNCx
-from TNCxMUSIC.utils.database import is_music_playing, music_on
-from TNCxMUSIC.utils.decorators import AdminRightsCheck
-from TNCxMUSIC.utils.inline import close_markup
+from TncMusic import app
+from TncMusic.core.call import TNC
+from TncMusic.utils.database import is_music_playing, music_on
+from TncMusic.utils.decorators import AdminRightsCheck
+from TncMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await TNCx.resume_stream(chat_id)
+    await TNC.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
