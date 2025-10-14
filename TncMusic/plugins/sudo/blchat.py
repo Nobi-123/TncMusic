2dacb79932abc25f80ba47b1a65a4efdf7454f99
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 20:43:01 +0530

    Update blchat.py

--- TncMusic/plugins/sudo/blchat.py
+++ TncMusic/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from TNCxMUSIC import app
-from TNCxMUSIC.misc import SUDOERS
-from TNCxMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from TNCxMUSIC.utils.decorators.language import language
+from TncMusic import app
+from TncMusic.misc import SUDOERS
+from TncMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from TncMusic.utils.decorators.language import language
 from config import BANNED_USERS
 
 
