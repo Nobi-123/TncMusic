3a0e5349dda52f7e78aa323321378a7f02fe5b7d
Nobi-123 <nobita.infringement.report@gmail.com>
2025-10-14 19:48:28 +0530

    Update sys.py

--- TncMusic/utils/sys.py
+++ TncMusic/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from TNCxMUSIC.misc import _boot_
-from TNCxMUSIC.utils.formatters import get_readable_time
+from TncMusic.misc import _boot_
+from TncMusic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
